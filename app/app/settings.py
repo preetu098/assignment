aeb4c4fb453aee2e2cb25ff1b0036a7a0e5739bb
Preetu098 <preetu098@gmail.com>
2023-02-05 01:34:22 +0530

    add

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -115,9 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+   STATIC_URL
+]
+STATIC_ROOT='/home/rawwieb8v1f2/public_html/workhelpnow/pro/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
