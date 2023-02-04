6b17018b9538118394adb7b13ee33b6565d94645
Preetu098 <preetu098@gmail.com>
2023-02-05 01:40:32 +0530

    add

--- app/app/settings.py
+++ app/app/settings.py
@@ -116,10 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-   os.path.join(BASE_DIR,STATIC_URL)
-]
-STATIC_ROOT='/home/rawwieb8v1f2/public_html/workhelpnow/static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 
