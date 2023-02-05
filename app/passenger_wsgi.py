cb0b8a2cfc82db791b7f1ace868ada7915b6b05f
Preetu098 <preetu098@gmail.com>
2023-02-06 00:03:47 +0530

    add chnages

--- app/passenger_wsgi.py
+++ app/passenger_wsgi.py
@@ -6,7 +6,7 @@ from django.core.wsgi import get_wsgi_application
 
 # Set up paths and environment variables
 sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myapp.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 
 # Set script name for the PATH_INFO fix below
 SCRIPT_NAME = os.getcwd()
