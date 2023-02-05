2cfff93b2651beb2f86595abc1a4ace7121a3e89
Preetu098 <preetu098@gmail.com>
2023-02-05 23:09:57 +0530

    add chnages

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,21 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from pro import views
+from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
-    path('dashboard/',views.dashboard),
-    path('mobile/',views.mobile),
-    path('pay/',views.payment),
-    path('buycoin/',views.getCoins),
-    path('coins/',views.coinsform),
-    path('coinstatus/',views.coinStatus),
-    path('promote/',views.promote),
-    path('invite/',views.invite),
-    path('search/',views.searchRecord),
-    path('requesttutor/',views.requestTutor),
-    path('register/',views.register),
-    path('assign/',views.assignmenthelp,name="assignmenthelp")
+    path('',include('pro.urls'))
+
 ]
---
+++ app/pro/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from pro import views
+urlpatterns = [
+    path('',views.index),
+    path('dashboard/',views.dashboard),
+    path('mobile/',views.mobile),
+    path('pay/',views.payment),
+    path('buycoin/',views.getCoins),
+    path('coins/',views.coinsform),
+    path('coinstatus/',views.coinStatus),
+    path('promote/',views.promote),
+    path('invite/',views.invite),
+    path('search/',views.searchRecord),
+    path('requesttutor/',views.requestTutor),
+    path('register/',views.register),
+    path('assign/',views.assignmenthelp,name="assignmenthelp")
+]
