aeab50b59eefe75af13eed14a1cde9f8b6082b7f
Preetu098 <preetu098@gmail.com>
2023-02-06 13:00:30 +0530

    add changes

--- app/pro/urls.py
+++ app/pro/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
 from pro import views
 urlpatterns = [
     path('',views.index),
-    path('dashboard/',views.dashboard),
-    path('mobile/',views.mobile),
+    re_path('dashboard/',views.dashboard),
+    re_path('mobile/',views.mobile),
     path('pay/',views.payment),
     path('buycoin/',views.getCoins),
     path('coins/',views.coinsform),
@@ -13,5 +13,5 @@ urlpatterns = [
     path('search/',views.searchRecord),
     path('requesttutor/',views.requestTutor),
     path('register/',views.register),
-    path('assign/',views.assignmenthelp,name="assign")
+    re_path('assign/',views.assignmenthelp,name="assign")
 ]
